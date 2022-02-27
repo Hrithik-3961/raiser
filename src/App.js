c2d8005cf6c59a2f1122947e491fecc72b968c46
Hrithik Agarwal <agarwal.hrithik3961@gmail.com>
2022-02-27 12:45:08 +0530

    Connected metamask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [metamaskInstalled, setMetamaskInstalled] = useState(false);
+  useEffect(() => {
+    if (
+      typeof window.ethereum !== "undefined" &&
+      typeof window.ethereum._eventsCount !== "undefined"
+    ) {
+      setMetamaskInstalled(true);
+    } else setMetamaskInstalled(false);
+  }, []);
+
+  const ALFAJORES_PARAMS = {
+    chainId: "0xaef3",
+    chainName: "Alfajores Testnet",
+    nativeCurrency: { name: "Alfajores Celo", symbol: "A-CELO", decimals: 18 },
+    rpcUrls: ["https://alfajores-forno.celo-testnet.org"],
+    blockExplorerUrls: ["https://alfajores-blockscout.celo-testnet.org/"],
+    iconUrls: ["future"],
+  };
+
+  async function connect() {
+    try {
+      if (typeof window.ethereum === "undefined") {
+      }
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [ALFAJORES_PARAMS],
+      });
+    } catch (error) {
+      if (error.code === 4001)
+        alert("Please connect to MetaMask to continue !");
+      else console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {metamaskInstalled === true ? (
+        <button onClick={connect}>Connect</button>
+      ) : (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Download and enable{" "}
+          <a
+            href="https://metamask.io/download/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Metamask
+          </a>{" "}
+          to proceed
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
